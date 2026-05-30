05448b9a9b345b5dc08e07035c6f98e5ecb0ea0c
Щелочінін артур <a.shchelochinin@gmail.com>
2026-05-30 12:52:01 +0300

    created

---
+++ app.py
@@ -0,0 +1,97 @@
+import time
+import tkinter as tk
+
+from constants import DROP_MS, STAR_BG
+from logic import GameLogic
+from screens import Screen, StartScreen, GameScreen
+
+
+class TetrisApp:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("✦ SPACE TETRIS ✦")
+        self.root.configure(bg=STAR_BG)
+        self.root.resizable(False, False)
+
+        self._logic: GameLogic | None = None
+        self._screen: Screen | None = None
+        self._drop_after = None
+        self._start_time = 0
+
+        self._show_start()
+
+    def _show_start(self):
+        if self._screen:
+            self._screen.destroy()
+        self._screen = StartScreen(self.root, on_start=self._start_game)
+        self._screen.build()
+
+    def _start_game(self):
+        if self._screen:
+            self._screen.destroy()
+
+        self._logic = GameLogic()
+        game_screen = GameScreen(self.root, on_restart=self._restart_game, on_menu=self._show_start)
+        game_screen.build()
+        self._screen = game_screen
+
+        self.root.bind_all("<Left>",  lambda e: self._logic.try_move(-1, 0))
+        self.root.bind_all("<Right>", lambda e: self._logic.try_move(1, 0))
+        self.root.bind_all("<Up>",    lambda e: self._logic.try_rotate())
+        self.root.bind_all("<Down>",  lambda e: self._handle_drop())
+
+        self._logic.spawn()
+        self._start_time = time.time()
+        self._schedule_drop()
+        self._update_timer()
+        self._draw_loop()
+
+    def _restart_game(self):
+        if self._drop_after:
+            self.root.after_cancel(self._drop_after)
+            self._drop_after = None
+
+        self._logic.reset()
+        self._logic.spawn()
+        self._start_time = time.time()
+        self._screen.update_score(0)
+        self._screen.update_time(0)
+        self._schedule_drop()
+        self._update_timer()
+
+    def _handle_drop(self):
+        if not self._logic or self._logic.game_over:
+            return
+        self._logic.hard_drop()
+        self._logic.lock_piece()
+        self._logic.spawn()
+        self._screen.update_score(self._logic.score)
+
+    def _schedule_drop(self):
+        if self._drop_after:
+            self.root.after_cancel(self._drop_after)
+        self._drop_after = self.root.after(DROP_MS, self._gravity_tick)
+
+    def _gravity_tick(self):
+        if not self._logic or self._logic.game_over:
+            return
+        locked = self._logic.gravity_step()
+        if locked:
+            self._logic.spawn()
+            self._screen.update_score(self._logic.score)
+        self._schedule_drop()
+
+    def _update_timer(self):
+        if not self._logic or self._logic.game_over:
+            return
+        elapsed = int(time.time() - self._start_time)
+        self._screen.update_time(elapsed)
+        self.root.after(1000, self._update_timer)
+
+    def _draw_loop(self):
+        if self._logic and isinstance(self._screen, GameScreen):
+            self._screen.draw(self._logic)
+        self.root.after(16, self._draw_loop)
+
+    def run(self):
+        self.root.mainloop()
